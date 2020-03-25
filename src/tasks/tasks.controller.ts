dd5c5aa0f71f935259973e0918c3c30123196be4
Ratatinator97 <asidiras.csi@gmail.com>
2020-03-25 19:29:54 +0100

    Added : Validation Pipes, Custom Pipes and Error Handling :)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 
 
@@ -11,7 +12,7 @@ export class TasksController {
     constructor(private tasksService: TasksService) {}
     
     @Get()
-    getTasks(@Query() filterDto:GetTasksFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto:GetTasksFilterDto): Task[] {
         if(Object.keys(filterDto).length){
             return this.tasksService.getTasksWithFilters(filterDto);
         }
@@ -30,6 +31,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
     }
@@ -37,7 +39,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id: string, 
-        @Body('status') status: TaskStatus,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
         ):Task {
             return this.tasksService.updateTaskStatus(id, status);
     }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -26,7 +26,12 @@ export class TasksService {
         return tasks;
     }
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if(!found){
+            throw new NotFoundException(`Task with id "${id}" not found`);
+        }
+        return found;
     }
     createTask(createTaskDto: CreateTaskDto): Task {
         const { title, description } = createTaskDto;
@@ -41,7 +46,9 @@ export class TasksService {
         return task;
     }
     deleteTaskById(id: string):void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        //this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
     updateTaskStatus(id: string, status: TaskStatus):Task {
         const task = this.getTaskById(id);
